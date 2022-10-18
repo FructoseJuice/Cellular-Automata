62c56f5ae86f94d2c75058478341fe67bcad7780
FructoseJuice <bzhidalgo@gmail.com>
2022-10-18 00:17:04 -0600

    Fixes

--- src/CellTypes/LangtonCell.java
+++ src/CellTypes/LangtonCell.java
@@ -43,31 +43,31 @@ public class LangtonCell extends Cell {
     //Helpers
     public Color inferStatusColor(int status) {
         switch (status) {
-            case 0 -> {
+            case 0: {
                 return Color.BLACK;
             }
-            case 1 -> {
+            case 1: {
                 return Color.YELLOW;
             }
-            case 2 -> {
+            case 2: {
                 return Color.PURPLE;
             }
-            case 3 -> {
+            case 3: {
                 return Color.BLUE;
             }
-            case 4 -> {
+            case 4: {
                 return Color.RED;
             }
-            case 5 -> {
+            case 5: {
                 return Color.GREEN;
             }
-            case 6 -> {
+            case 6: {
                 return Color.ORANGE;
             }
-            case 7 -> {
+            case 7: {
                 return Color.BROWN;
             }
-            default -> {
+            default: {
                 return null;
             }
         }
